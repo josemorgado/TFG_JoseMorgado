5fb83d533e16c06e20eae325e392c4dde2f52d6c
josemorgado <josemaria1.jmmp@gmail.com>
2026-01-08 18:17:18 +0100

    feat(queja): update attributes in Complaint model

--- backend/config/quejas/models.py
+++ backend/config/quejas/models.py
@@ -15,11 +15,10 @@ class Queja(models.Model):
     #categoria_id = models.ForeignKey('Categoria', on_delete=models.CASCADE)
     #distrito_id = models.ForeignKey('Distrito', on_delete=models.CASCADE)
     estado = models.CharField(max_length=3,choices= EstadoQueja.choices, default=EstadoQueja.PENDIENTE)
-    ubicacion = models.CharField(max_length=255)
+    ubicacion = models.CharField(max_length=255,blank=True, null=True)
     #autor_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_creacion = models.DateTimeField(auto_now=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
     num_votos = models.IntegerField(default=0)
     num_comentarios = models.IntegerField(default=0)
-    
     
