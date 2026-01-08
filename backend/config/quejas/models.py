fb00cfefc35ced60a2614c7af3820b47bf3a1af5
josemorgado <josemaria1.jmmp@gmail.com>
2026-01-08 19:15:11 +0100

    feat(queja): update Queja model

--- backend/config/quejas/models.py
+++ backend/config/quejas/models.py
@@ -17,7 +17,7 @@ class Queja(models.Model):
     estado = models.CharField(max_length=3,choices= EstadoQueja.choices, default=EstadoQueja.PENDIENTE)
     ubicacion = models.CharField(max_length=255,blank=True, null=True)
     #autor_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    fecha_creacion = models.DateTimeField(auto_now=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
     num_votos = models.IntegerField(default=0)
     num_comentarios = models.IntegerField(default=0)
