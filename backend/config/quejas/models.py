51449276a66462d40ad4e0c812e7281774181cdf
josemorgado <josemaria1.jmmp@gmail.com>
2026-01-09 13:15:15 +0100

    feat(queja): update the model to caculate the num_votos correctly

--- backend/config/quejas/models.py
+++ backend/config/quejas/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from megusta.models import MeGusta
+
+
 class EstadoQueja(models.TextChoices):
         PENDIENTE = 'PEN', 'Pendiente'
         EN_PROGRESO = 'ENP', 'En Progreso'
@@ -16,9 +22,14 @@ class Queja(models.Model):
     distrito = models.ForeignKey('distrito.Distrito', on_delete=models.CASCADE, related_name='quejas', null=False, blank=False)
     estado = models.CharField(max_length=3,choices= EstadoQueja.choices, default=EstadoQueja.PENDIENTE)
     ubicacion = models.CharField(max_length=255,blank=True, null=True)
-    #autor_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
-    num_votos = models.IntegerField(default=0)
+    #Este campo se actualizará mediante señales cuando se añadan o eliminen MeGusta o Comentarios
+    #No se guarda en la base de datos, se calcula al vuelo
+    @property
+    def num_votos(self):
+        ct = ContentType.objects.get_for_model(self, for_concrete_model=False)
+        return MeGusta.objects.filter(content_type=ct, object_id=self.pk).count()
     num_comentarios = models.IntegerField(default=0)
     
