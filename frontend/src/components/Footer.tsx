c55e14d4e1e37dd1b54ce81e7d65a97c75e6ad24
josemorgado <josemaria1.jmmp@gmail.com>
2026-03-26 11:18:42 +0100

    feat(footer): Some changes in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 const Footer = () => {
@@ -16,7 +15,7 @@ const Footer = () => {
 
 
           {/* Correo */}
-          <a href="mailto:soporte@alcaldeescuchame.com" title="Enviar correo">
+          <a href="mailto:010Lineasevilla@sevilla.org" title="Enviar correo">
             <svg className="icon" viewBox="0 0 24 24">
               <path fill="currentColor"
                 d="M4 4h16a2 2 0 012 2v1l-10 6L2 7V6a2 2 0 012-2zm18 5l-10 6L2 9v9a2 2 0 002 2h16a2 2 0 002-2V9z"/>
@@ -24,7 +23,7 @@ const Footer = () => {
           </a>
 
           {/* X (Twitter) */}
-          <a href="https://x.com/X" target="_blank" rel="noopener noreferrer">
+          <a href="https://x.com/Ayto_Sevilla" target="_blank" rel="noopener noreferrer">
             <svg className="icon" viewBox="0 0 24 24">
               <path fill="currentColor"
                 d="M4 4l7.5 8.5L4 20h3l6-7 6 7h3l-7.5-7.5L20 4h-3l-6 7-6-7H4z"/>
@@ -32,7 +31,7 @@ const Footer = () => {
           </a>
 
           {/* Instagram */}
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/ayto_sevilla/" target="_blank" rel="noopener noreferrer">
             <svg className="icon" viewBox="0 0 24 24">
               <path fill="currentColor"
                 d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm5 5a5 5 0 100 10 5 5 0 000-10zm6.5-1.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"/>
