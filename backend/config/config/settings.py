c0c030d86faedead8d3081006ca83e40b7154b18
josemorgado <josemaria1.jmmp@gmail.com>
2026-04-14 17:24:53 +0200

    feat(release): Some changes

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -111,17 +111,15 @@ TEMPLATES = [
 # DATABASE
 # ======================
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # ======================
 # AUTH
