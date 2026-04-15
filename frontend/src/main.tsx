2f8d9a361e99c81683878bfc527fddf2a806df4b
josemorgado <josemaria1.jmmp@gmail.com>
2026-04-15 11:29:01 +0200

    feat(frontend): Frontend listo para proudccion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import React from "react";
 import App from "./App";
 import "./index.css";
 import "./styles/base.css";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { NotificationsProvider } from "./context/NotificationsContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 
