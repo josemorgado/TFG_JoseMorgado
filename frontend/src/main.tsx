f1f0084462bc800c0ad26092be12093d37455ac9
josemorgado <josemaria1.jmmp@gmail.com>
2026-04-27 18:37:50 +0200

    feat(Deploy)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,35 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
-import App from "./App";
-import "./index.css";
-import "./styles/base.css";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import AppRouter from "./routes/AppRouter";
 import { AuthProvider } from "./context/AuthContext";
 import { NotificationsProvider } from "./context/NotificationsContext";
+
+import "./index.css";
+import "./styles/base.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <NotificationsProvider>
+            <AppRouter />
+          </NotificationsProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
 
 console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
-
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <NotificationsProvider>
-          <App />
-        </NotificationsProvider>
-      </AuthProvider>
-    </QueryClientProvider>
-  </StrictMode>
-);
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,63 +1,59 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import Home from '../pages/Home';
-import Register from '../pages/Register';
-import Layout from '../components/Layout';
-import QuejaCreate from '../pages/QuejaCreate';
-import QuejaUpdate from '../pages/QuejaUpdate';
-import PrivateRouteCrearQueja from './PrivateRouteCrearQueja';
-import QuejaDetail from '../pages/QuejaDetail';
-import PerfilDetail from '../pages/PerfilDetail';
-import PerfilUpdate from '../pages/PerfilUpdate';
-import PrivateRoute from './PrivateRoute';
-import RutaProhibida from '../pages/RutaProhibida';
-import ChangePassword from '../pages/ChangePassword';
-import QuejasList from '../pages/QuejasList';
-import ResetPassword from '../pages/ResetPassword';
-import EnterToken from '../pages/EnterToken';
-import NewPassword from '../pages/NewPassword';
-import Stats from '../pages/Stats';
-import Notificaciones from '../pages/Notificaciones';
-import QuejaRespuestasPage from '../pages/QuejaRespuestas';
-import QuejaResponder from '../pages/QuejaResponder';
-import Contacto from '../pages/Contacto';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Layout from "../components/Layout";
+import QuejaCreate from "../pages/QuejaCreate";
+import QuejaUpdate from "../pages/QuejaUpdate";
+import PrivateRouteCrearQueja from "./PrivateRouteCrearQueja";
+import QuejaDetail from "../pages/QuejaDetail";
+import PerfilDetail from "../pages/PerfilDetail";
+import PerfilUpdate from "../pages/PerfilUpdate";
+import PrivateRoute from "./PrivateRoute";
+import RutaProhibida from "../pages/RutaProhibida";
+import ChangePassword from "../pages/ChangePassword";
+import QuejasList from "../pages/QuejasList";
+import ResetPassword from "../pages/ResetPassword";
+import EnterToken from "../pages/EnterToken";
+import NewPassword from "../pages/NewPassword";
+import Stats from "../pages/Stats";
+import Notificaciones from "../pages/Notificaciones";
+import QuejaRespuestasPage from "../pages/QuejaRespuestas";
+import QuejaResponder from "../pages/QuejaResponder";
+import Contacto from "../pages/Contacto";
 
 const AppRouter: React.FC = () => (
-  <BrowserRouter>
-    <Routes>
+  <Routes>
+    <Route element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/quejas/" element={<QuejasList />} />
+      <Route path="/quejas/:id" element={<QuejaDetail />} />
+      <Route path="/quejas/:id/update" element={<QuejaUpdate />} />
+      <Route path="/perfil/:id" element={<PerfilDetail />} />
+      <Route path="/contact" element={<Contacto />} />
+      <Route path="/ruta-prohibida" element={<RutaProhibida />} />
+      <Route path="/reset-password/" element={<ResetPassword />} />
+      <Route path="/enter-token/" element={<EnterToken />} />
+      <Route path="/new-password/" element={<NewPassword />} />
+      <Route path="/stats/" element={<Stats />} />
+      <Route path="/notificaciones/" element={<Notificaciones />} />
+      <Route path="/quejas/:quejaId/respuestas" element={<QuejaRespuestasPage />} />
+      <Route path="/quejas/:quejaId/responder" element={<QuejaResponder />} />
 
-      <Route element={<Layout />}>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/quejas/:id" element={<QuejaDetail />} />
-        <Route path="/perfil/:id" element={<PerfilDetail />} />
-        <Route path="/contact" element={<Contacto />} />
-        <Route path="/ruta-prohibida" element={<RutaProhibida />} />
-        <Route path="/quejas/:id/update" element={<QuejaUpdate />} />
-        <Route path="/quejas/" element={<QuejasList />} />
-        <Route path="/reset-password/" element={<ResetPassword />} />
-        <Route path="/enter-token/" element={<EnterToken />} />
-        <Route path="/new-password/" element={<NewPassword />} />
-        <Route path="/stats/" element={<Stats />} />
-        <Route path="/notificaciones/" element={<Notificaciones />} />
-        <Route path="/quejas/:quejaId/respuestas" element={<QuejaRespuestasPage />} />
-        <Route path="/quejas/:quejaId/responder" element={<QuejaResponder />} />
-        {/* Rutas privadas */}
-        <Route element={<PrivateRouteCrearQueja />}>
-          <Route path="/create-queja" element={<QuejaCreate />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="perfil/:id/update/" element={<PerfilUpdate />} />
-          <Route path="perfil/:id/change-password/" element={<ChangePassword />} />
-        </Route>
-
+      <Route element={<PrivateRouteCrearQueja />}>
+        <Route path="/create-queja" element={<QuejaCreate />} />
       </Route>
 
-    </Routes>
-  </BrowserRouter>
+      <Route element={<PrivateRoute />}>
+        <Route path="/perfil/:id/update/" element={<PerfilUpdate />} />
+        <Route path="/perfil/:id/change-password/" element={<ChangePassword />} />
+      </Route>
+    </Route>
+  </Routes>
 );
 
 export default AppRouter;
