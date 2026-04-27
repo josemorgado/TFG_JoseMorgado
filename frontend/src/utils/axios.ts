3b452b28cf7b5d0f0dd9023bee00f1814c30b355
josemorgado <josemaria1.jmmp@gmail.com>
2026-04-27 19:05:59 +0200

    feat(Deploy)

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { storage } from "./storage";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL+"/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 if (!BASE_URL) {
   throw new Error("VITE_API_BASE_URL no está definida");
