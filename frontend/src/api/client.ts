b2840cbab2fa9b52c18d4179e95e67715078fc34
josemorgado <josemaria1.jmmp@gmail.com>
2026-04-28 10:04:04 +0200

    feat(Deploy)

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,16 +1,4 @@
-import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ?? "https://alcalde-escuchame-backend.onrender.com";
+import api from "../utils/axios";
 
-export const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    console.error("API Error:", err.response || err);
-    throw err;
-  }
-);
+export { api };
