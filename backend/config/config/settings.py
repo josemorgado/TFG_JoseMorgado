5d99b4a1880eac299991623c4b3e2819b403c93a
josemorgado <josemaria1.jmmp@gmail.com>
2026-05-16 18:28:01 +0200

    feat(deploy)

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -112,12 +112,18 @@ TEMPLATES = [
 # ======================
 
 
+
 if config('DJANGO_ENV', default='local') == 'production':
     DATABASES = {
         'default': dj_database_url.config(
-            conn_max_age=600
+            conn_max_age=600,
         )
     }
+
+    DATABASES['default']['OPTIONS'] = {
+        'sslmode': 'require'
+    }
+
 else:
     DATABASES = {
         'default': {
